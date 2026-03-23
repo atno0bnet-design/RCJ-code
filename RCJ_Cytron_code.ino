d0646ee6155b7fc933411fca0fa6bedb2c5f6d4b
atno0bnet-design <atno0b.net@gmail.com>
2026-03-22 20:48:16 -0400

    Add files via upload

--- RCJ_Cytron_code.ino
+++ RCJ_Cytron_code.ino
@@ -3,6 +3,11 @@
 #define PIN 7
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
+
+int condition = 0;
+int L_motor = 0;
+int R_motor = 0;
+
 enum bub{l1=50,l2=50,r1=50,r2=50};
 int cmap(int val){
   int mapped = map(val, 0 , 100, 0, 255);
@@ -22,7 +27,7 @@ void motor::speed(int val){
   int map_speed = cmap(abs(val));
     if(val>0){
       analogWrite(fpin, map_speed);
-      analogWrite(rpin, 0);
+      analogWrite(rpin, 0); 
     }
     else{
       analogWrite(fpin, 0);
@@ -44,25 +49,41 @@ void stop(int time)
   
 }
 
+
+void move(int a, int b){
+  motorR1.speed(a);
+  motorR2.speed(a);
+  motorL1.speed(b);
+  motorR2.speed(b);
+}
+
 void setup() {
+
+  stop(0);
   Serial.begin(115200);
   Serial.println("Hello");
+  Serial1.setRX(17);
+  Serial1.setTX(16);
+  Serial1.begin(115200);
+  delay(2000);
+  
   pinMode(20, INPUT);
+  while(digitalRead(20)==0);
   pixels.begin();
   pixels.setBrightness(50);
   for(int i = 0;i<NUMPIXELS;i++){
     pixels.setPixelColor(i, pixels.Color(100,100,100));
   }
   pixels.show();
-  while(digitalRead(20)==1);
-    motorR1.speed(60);
-    motorR2.speed(60);
-    motorL1.speed(0);
-    motorL2.speed(0);
-    Serial.println("Hi");
-
+  Serial1.write('a');
 }
 
 void loop(){
+  if(Serial1.available() >0){
+        condition = Serial1.parseInt();
+        R_motor = Serial1.parseInt();
+        L_motor = Serial1.parseInt();
+        move(R_motor,L_motor);
+  }
   
 }
